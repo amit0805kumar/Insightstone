ef04a89003aaeebd43e60f8ad76fa5038af2e130
Amit <amit0805kumar@gmail.com>
2018-08-09 15:23:09 +0530

    scroll effects and footer added
    Project merged

---
+++ js/script.js
@@ -0,0 +1,65 @@
+function active(){
+    document.querySelector(".right__container").classList.toggle('active-form');
+}
+
+
+$(function(){
+
+	$.validator.addMethod( "alphanumeric", function( value, element ) {
+	return this.optional( element ) 
+	|| /^\w+$/i.test( value )
+	&& value.length>=3
+	&& value.length<=40;
+    }, "Please Enter a Valid Input" );
+
+    $.validator.addMethod( "invalid_characters", function( value, element ) {
+	return this.optional( element ) || /^[^&<>'"]+$/i.test( value );
+    }, "Please Enter a Valid Input" );
+
+    $.validator.setDefaults({
+    errorClass: 'text-danger'
+  });
+
+$("#join_us_form").validate({
+        rules: {
+            
+			name: {
+                required: true,
+                invalid_characters:true
+             
+            },
+			email: {
+				required:true,
+				invalid_characters:true
+			},
+			contact_number: {
+				required:true,
+				invalid_characters:true,
+				digits:true
+			},
+			college: {
+                required: true,
+                invalid_characters:true
+            },
+            profile_link: {
+                required: true,
+                invalid_characters:true,
+                url:true
+            },
+            followers: {
+                required: true,
+                invalid_characters:true,
+                digits:true
+            }						
+        },
+        messages:{
+		     name :{
+				alphanumeric:'Please Enter a Valid Name'
+			},
+			profile_link :{
+				url:'Please Enter a Valid Profile Link'
+			}
+
+		}
+    });
+});
--- script.js
+++
@@ -1,3 +0,0 @@
-function active(){
-    document.querySelector(".right__container").classList.toggle('active-form');
-}
