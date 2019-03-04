7e9aac599f35b8ea8620eb84a807976aeb037ad4
Amit Kumar <32258879+amit0805kumar@users.noreply.github.com>
2019-03-04 22:07:08 +0530

    comment removed

--- blog/db.php
+++ blog/db.php
@@ -1,5 +1,5 @@
 <?php
-//$connect=mysqli_connect("csi.crq0gw48mcqg.us-east-2.rds.amazonaws.com","csi999","xhxh44xh","insightone");
+
 $connect=mysqli_connect("localhost","root","","insightone");
 if (!$connect) {
 	# code...
